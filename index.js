492d2bdecad764e677810767e2f2c0cd9b0558f0
AmarJeetVerma9151 <vermaamarjeet095@gmail.com>
2025-07-10 16:37:40 +0530

    update

--- index.js
+++ index.js
@@ -14,19 +14,19 @@ dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
 
-// const corsOptions = {
-//   origin: "https://chat-application-29sm.vercel.app",
-//   methods: ["GET", "POST"],
-//   credentials: true,
-// };
+const corsOptions = {
+  origin: "https://chat-application-29sm.vercel.app",
+  methods: ["GET", "POST"],
+  credentials: true,
+};
 
-app.use(cors({
-  origin: 'https://chat-application-29sm.vercel.app', // frontend का URL
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+// app.use(cors({
+//   origin: 'https://chat-application-29sm.vercel.app', // frontend का URL
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   credentials: true
+// }));
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 // ✅ Handle preflight requests (very important)
 // app.options("*", cors(corsOptions));
